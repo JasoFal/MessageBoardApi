8668bbefafa5e4c36ef3bef427b64f8208a683b4
Jason Falk <jasonfalk9@gmail.com>
2024-05-31 15:46:45 -0700

    Add Put route.

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -36,6 +36,44 @@ namespace MessageBoard.Controllers
       return message;
     }
 
-    
+    [HttpPost]
+    public async Task<ActionResult<Message>> Post(Message message)
+    {
+      _db.Messages.Add(message);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetMessage), new { id = message.MessageId }, message);
+    }
+
+    // PUT: http://localhost:5000/api/messages/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Message message)
+    {
+      if (id != message.MessageId)
+      {
+        return BadRequest();
+      }
+      _db.Messages.Update(message);
+      try
+      {
+        await _db.SaveChanges();
+      }
+      catch
+      {
+        if (!MessageExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    private bool MessageExists(int id)
+    {
+      return _db.Messages.Any(e => e.MessageId == id);
+    }
   }
 }
