0f2c5a298001a7d34ba8b880bc45acc72a7eedc3
Jason Falk <jasonfalk9@gmail.com>
2024-06-03 15:45:01 -0700

    Add seed data.

--- Models/MessageBoardContext.cs
+++ Models/MessageBoardContext.cs
@@ -10,12 +10,14 @@ namespace MessageBoard.Models
     {
     }
 
-    // protected override void OnModelCreating(ModelBuilder builder)
-    // {
-    //   builder.Entity<Message>()
-    //     .hasData(
-    //       new Message
-    //   );
-    // }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Message>()
+        .HasData(
+          new Message { MessageId = 1, Content = "What's up", Group = "Video", user_name = "jon"},
+          new Message { MessageId = 2, Content = "Not much", Group = "Video", user_name = "join"},
+          new Message { MessageId = 3, Content = "The weather", Group = "Weather", user_name = "Mr.Weather"}
+      );
+    }
   }
 }
