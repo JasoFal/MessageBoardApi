86dd8f031217ff4572fff05f921b1fe3b29fd977
Jason Falk <jasonfalk9@gmail.com>
2024-06-03 15:44:31 -0700

    Add database seed data.

---
+++ Migrations/20240603223400_SeedData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessageBoard.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "Content", "Group", "PostTime", "user_name" },
+                values: new object[] { 1, "What's up", "Video", new DateTime(2024, 6, 3, 15, 34, 0, 424, DateTimeKind.Local).AddTicks(491), "jon" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "Content", "Group", "PostTime", "user_name" },
+                values: new object[] { 2, "Not much", "Video", new DateTime(2024, 6, 3, 15, 34, 0, 424, DateTimeKind.Local).AddTicks(524), "join" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "Content", "Group", "PostTime", "user_name" },
+                values: new object[] { 3, "The weather", "Weather", new DateTime(2024, 6, 3, 15, 34, 0, 424, DateTimeKind.Local).AddTicks(526), "Mr.Weather" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 3);
+        }
+    }
+}
